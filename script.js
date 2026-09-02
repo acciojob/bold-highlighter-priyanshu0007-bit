3a4d2cd8e7266786f7aa59feeef68b1989499243
priyanshu ahir <priyanshuahir777@gmail.com>
2026-09-02 13:42:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
-
-
+	const strongElement = document.querySelectorAll("strong");
+	strongElement.forEach(elem =>{
+		elem.style.color = 'rgb(0,128,0)';
+	})
 }
 
 
 function return_normal() {
     //Write your code here
-
+	const strongElement = document.querySelectorAll("strong");
+	strongElement.forEach(elem =>{
+		elem.style.color = 'rgb(0,0,0)';
+	})
     
 }
